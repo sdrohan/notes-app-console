4528a6cfa2e12a7eae6f18140c4fbfa2a643cdf4
Siobhan Drohan <sdrohan@wit.ie>
2021-12-14 19:02:07 +0000

    New Functionality to List by Status - Active vs Archived (including tests)

--- src/main/kotlin/controllers/NoteAPI.kt
+++ src/main/kotlin/controllers/NoteAPI.kt
@@ -21,9 +21,60 @@ class NoteAPI {
         }
     }
 
+    fun listActiveNotes(): String {
+        return if (numberOfActiveNotes() == 0) {
+            "No active notes stored"
+        } else {
+            var listOfActiveNotes = ""
+            for (note in notes) {
+                if (!note.isNoteArchived) {
+                    listOfActiveNotes += "${notes.indexOf(note)}: $note \n"
+                }
+            }
+            listOfActiveNotes
+        }
+    }
+
+    fun listArchivedNotes(): String {
+        return if (numberOfArchivedNotes() == 0) {
+            "No archived notes stored"
+        } else {
+            var listOfArchivedNotes = ""
+            for (note in notes) {
+                if (note.isNoteArchived) {
+                    listOfArchivedNotes += "${notes.indexOf(note)}: $note \n"
+                }
+            }
+            listOfArchivedNotes
+        }
+    }
+
     fun numberOfNotes(): Int {
         return notes.size
     }
+
+    fun numberOfArchivedNotes(): Int {
+        //return notes.stream().filter { obj: Note -> obj.isNoteArchived }.count().toInt()
+        var counter = 0
+        for (note in notes) {
+            if (note.isNoteArchived) {
+                counter++
+            }
+        }
+        return counter
+    }
+
+    fun numberOfActiveNotes(): Int {
+        //return notes.stream().filter { p: Note -> !p.isNoteArchived }.count().toInt()
+        var counter = 0
+        for (note in notes) {
+            if (!note.isNoteArchived) {
+                counter++
+            }
+        }
+        return counter
+    }
+
 
     fun findNote(index: Int): Note? {
         return if (isValidListIndex(index, notes)) {
