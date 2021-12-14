8b7034ab017254e3d6d8915d9378e1519d537194
Siobhan Drohan <sdrohan@wit.ie>
2021-12-14 13:45:25 +0000

    Adding add and list for notes collection

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,13 @@
+import controllers.NoteAPI
+import models.Note
 import mu.KotlinLogging
-import utils.ScannerInput
+import utils.ScannerInput.readNextInt
+import utils.ScannerInput.readNextLine
 import java.lang.System.exit
 
 private val logger = KotlinLogging.logger {}
 
+private val noteAPI = NoteAPI()
 
 fun main(args: Array<String>) {
     runMenu()
@@ -24,7 +28,7 @@ fun runMenu() {
 }
 
 fun mainMenu() : Int {
-    return ScannerInput.readNextInt(""" 
+    return readNextInt(""" 
          > ----------------------------------
          > |        NOTE KEEPER APP         |
          > ----------------------------------
@@ -40,12 +44,24 @@ fun mainMenu() : Int {
 }
 
 fun addNote(){
-    logger.info { "addNote() function invoked" }
+    //logger.info { "addNote() function invoked" }
+    val noteTitle = readNextLine("Enter a title for the note: ")
+    val notePriority = readNextInt("Enter a priority (1-low, 2, 3, 4, 5-high): ")
+    val noteCategory = readNextLine("Enter a category for the note: ")
+    val isAdded = noteAPI.add(Note(noteTitle, notePriority, noteCategory, false))
+
+    if (isAdded) {
+        println("Added Successfully")
+    } else {
+        println("Add Failed")
+    }
 }
 
 fun listNotes(){
-    logger.info { "listNotes() function invoked" }
+    //logger.info { "listNotes() function invoked" }
+    println(noteAPI.listAllNotes())
 }
+
 
 fun updateNote(){
     logger.info { "updateNotes() function invoked" }
